7cfbdb731e2c34b5be1276ba4c27c9dc1c819e83
Eliot Eshelman <eliot@microway.com>
2016-10-31 12:26:36 -0400

    Cleanup output of job start script

--- dependencies/etc/slurm/scripts/slurm.jobstart_messages.sh
+++ dependencies/etc/slurm/scripts/slurm.jobstart_messages.sh
@@ -3,7 +3,7 @@
 ######################## Microway Cluster Management Software (MCMS) for OpenHPC
 ################################################################################
 #
-# Copyright (c) 2015 by Microway, Inc.
+# Copyright (c) 2015-2016 by Microway, Inc.
 #
 # This file is part of Microway Cluster Management Software (MCMS) for OpenHPC.
 #
@@ -27,69 +27,93 @@
 #
 # Provide helpful messages for the start and end of a batch job.
 #
-# This 'library' script should not be called directly.
-# Include it in your scripts with:
-#    source slurm.jobstart_messages.sh
-#
 ################################################################################
 
+# Exit if this script isn't actually running within a SLURM context
+if [[ -z "$SLURM_JOB_UID" ]] || [[ -z "$SLURM_JOB_ID" ]]; then
+    echo "Do not run this script manually - it is used by SLURM"
+    exit 1
+fi
 
-echo Script started at $(date +"%F %T")
-echo Job ID number $SLURM_JOBID
-echo Starting from host $(hostname)
-echo
-echo The following compute nodes will be used: $SLURM_NODELIST
-echo
+
+echo "
+################################################################################
+# JOB DETAILS
+#
+# Job started at $(date +"%F %T")
+# Job ID number: $SLURM_JOBID
+#
+# Starting from host: $(hostname)
+# The following compute nodes will be used: $SLURM_NODELIST
+#"
 
 NPROCS=$SLURM_NTASKS
 NODES=$SLURM_JOB_NUM_NODES
 NUM_SOCKETS=$((`grep 'physical id' /proc/cpuinfo | sort -u | tail -n1 | cut -d" " -f3` + 1))
 NUM_CORES=$(grep siblings /proc/cpuinfo | head -n1 | cut -d" " -f2)
 
-echo Using $NPROCS processors across $NODES nodes
-echo $NUM_SOCKETS CPU sockets with $NUM_CORES cores each -- $(grep -m1 'model name' /proc/cpuinfo)
-echo System memory: $(grep MemTotal /proc/meminfo)
-echo
+echo "#
+# Using $NPROCS processes across $NODES nodes.
+# Reserving $SLURM_MEM_PER_NODE MB of memory per node.
+#
+# The node starting this job has:
+#
+# $NUM_SOCKETS CPU sockets with $NUM_CORES cores each -- $(grep -m1 'model name' /proc/cpuinfo)
+# System memory: $(awk '/MemTotal/ {print $2 $3}' /proc/meminfo)
+#"
 
 # Check for GPUs and print their status
 if [[ -n "$CUDA_VISIBLE_DEVICES" && "$CUDA_VISIBLE_DEVICES" != "NoDevFiles" ]]; then
     GPUS_PER_NODE=$(echo $CUDA_VISIBLE_DEVICES | sed 's/,/ /g' | wc --words)
 
-    # This assumes the nodes are homogeneous (all GPUs are the same type)
     first_index=$(echo $CUDA_VISIBLE_DEVICES | sed 's/,.*//')
     GPU_TYPE=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader --id=$first_index | sed 's/ /_/g')
 
-    echo NVIDIA CUDA device IDs in use: $CUDA_VISIBLE_DEVICES
-    echo
-    nvidia-smi
-    echo
+    echo "#
+# NVIDIA CUDA device IDs in use: $CUDA_VISIBLE_DEVICES
+#
+# Full list of GPU devices
+# $(nvidia-smi)
+#"
 fi
 
 # Check for Xeon Phi Coprocessors and print their status
 if [[ -n "$OFFLOAD_DEVICES" ]]; then
-    echo Xeon Phi device IDs in use: $OFFLOAD_DEVICES
-    echo
-    micinfo
-    echo
+    echo "#
+# Xeon Phi device IDs in use: $OFFLOAD_DEVICES
+#
+# $(micinfo)
+#"
 fi
 
 
 # Check for storage devices
 STORAGE_DEVICES=$(awk '!/Attached devices/' /proc/scsi/scsi)
 if [[ -n "$STORAGE_DEVICES" ]]; then
-    echo 'Locally-attached storage devices on this node:'
-    echo "$STORAGE_DEVICES"
+    echo "#
+# Storage devices attached to this node:
+# $STORAGE_DEVICES
+#"
 else
-    echo 'No storage devices are directly attached to this node.'
+    echo "#
+# No storage devices are attached to this node.
+#"
 fi
-echo; echo;
 
 
-# Change to the working directory
-echo Changing to working directory $SLURM_SUBMIT_DIR
-cd $SLURM_SUBMIT_DIR
-echo; echo;
+echo "#
+# Changing to working directory $SLURM_SUBMIT_DIR
+#
+################################################################################
 
+"
+
+
+################################################################################
+#
+# The section below will be run when the job has finished
+#
+################################################################################
 
 # Trap all exits (both with and without errors)
 trap exit_handler EXIT
@@ -99,15 +123,27 @@ trap exit ERR INT TERM
 
 exit_handler() {
     local error_code="$?"
-
-    # Show final information:
-    echo Script finished at $(date +"%F %T")
+    local exit_time=$(date +'%F %T')
 
     # If there was an error, report it.
     if [ "$error_code" -gt 0 ]; then
-        echo
-        echo WARNING! Script exited abnormally with error code: $error_code
-        echo
+        echo "
+
+################################################################################
+#
+# WARNING! Job exited abnormally at $exit_time with error code: $error_code
+#
+################################################################################"
+
+    # If the job completed successfully, report success.
+    else
+        echo "
+
+################################################################################
+#
+# Job finished successfully at $exit_time
+#
+################################################################################"
     fi
 
     exit $error_code
